d5d61a9720bd2556cd811046bbe27083832fac5b
krissnnaa <krissnnaa1988@gmail.com>
2019-11-27 13:26:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import AgglomerativeClustering
-import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
+import operator
+from collections import Counter
+from sklearn.metrics.pairwise import cosine_similarity
+from yellowbrick.cluster import KElbowVisualizer
 
-def dataPreprocessing():
+def dataPreprocessing(filename):
     """
     Encoding of raw data
     :return:
     """
-
     website = []
     userInteraction = {}
-    with open('test.txt', 'r') as file:
+    with open(filename, 'r') as file:
         user = 0
         interaction = []
         for line in file:
@@ -32,161 +31,160 @@ def dataPreprocessing():
 
     sortedWebsite = np.sort(np.asarray(website))
     userWeb = {}
-    for user, item in userInteraction.items():
-        temp = np.zeros(len(sortedWebsite))
-        for website in item:
-            index = np.where(sortedWebsite == website)
-            temp[index] = 1
+    if filename.startswith('train')==True:
+        for user, item in userInteraction.items():
+            temp = np.zeros(len(sortedWebsite))
+            for website in item:
+                index = np.where(sortedWebsite == website)
+                temp[index] = 1
+            userWeb[user] = temp.tolist()
+        return userWeb, userInteraction
+    else:
+        testLabel={}
+        for user, item in userInteraction.items():
+            temp = np.zeros(len(sortedWebsite))
+            if len(item)==1:
+                testLabel[user]=item[0]
+                index = np.where(sortedWebsite == item[0])
+                temp[index] = 1
+            else:
+                testLabel[user] = item[len(item)-1]
+                for website in item[:len(item)-1]:
+                    index = np.where(sortedWebsite == website)
+                    temp[index] = 1
+            userWeb[user] = temp.tolist()
+        return userWeb,testLabel
 
-        userWeb[user] = temp.tolist()
-    return userWeb,userInteraction
 
 
-def kmeanClustering(X_train, X_val, y_train, y_val):
+
+def userBasedNeighboring(X_train,userInteract,X_test, testUser):
+    #Training
+    cosine=cosine_similarity(X_train)
+    indx=np.argsort(cosine,axis=1)
+    similarTen={}
+    for user,simUser in zip(userInteract.keys(),indx):
+        similarTen[user]=list(simUser[len(X_train)-10:])
+    predictedWeb={}
+    for simUs,simVal in similarTen.items():
+        temp=[]
+        for item in simVal:
+            item=(10001+int(item))
+            temp.append(userInteract[item])
+        flat_list = [item for sublist in temp for item in sublist]
+
+        count=dict(Counter(flat_list))
+        clusteList = sorted(count.items(), key=lambda kv: kv[1])
+        finalList = []
+        for web, count in clusteList[len(clusteList) - 10:]:
+            finalList.append(web)
+        predictedWeb[simUs]= finalList #max(count.items(), key=operator.itemgetter(1))[0]
+
+    #Testing
+    testCosine=cosine_similarity(X_test,X_train)
+    indx = np.argsort(testCosine, axis=1)
+    finalIndex=indx[:,-1:]
+    truePredict=0
+    i=10001
+    for ind in finalIndex:
+        for web in predictedWeb[int(ind)+10001]:
+            if int (web)==int(testUser[i]):
+                truePredict+=1
+        i+=1
+
+    testAccuracy = truePredict / 5000
+
+    print('Test accuracy using user-based neighboring method=')
+    print(testAccuracy)
+
+
+def kmeanClustering(X_train,userInteract,X_test, testUser):
     """
     K-mean implementation
     :return:
     """
-    kmeans = KMeans(n_clusters=5, random_state=0).fit(X_train)
+    #Training
+    kmeans = KMeans(n_clusters=7, random_state=0).fit(X_train)
     kmeanLabels = kmeans.labels_
-    indexOfclass = np.where(kmeanLabels == 0)
-    clusterOne = y_train[indexOfclass]
-    indexOfclass = np.where(kmeanLabels == 1)
-    clusterTwo = y_train[indexOfclass]
-    indexOfclass = np.where(kmeanLabels == 2)
-    clusterThree = y_train[indexOfclass]
-    indexOfclass = np.where(kmeanLabels == 3)
-    clusterFour = y_train[indexOfclass]
-    indexOfclass = np.where(kmeanLabels == 4)
-    clusterFive = y_train[indexOfclass]
-    y_pred = kmeans.predict([X_val[3]])
-    return y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive
+    predictDict={}
+    indexOfclass = np.asarray(np.where(kmeanLabels == 0))
+    predictDict[0] = predictWebsites(userInteract,indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 1))
+    predictDict[1] = predictWebsites(userInteract, indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 2))
+    predictDict[2] = predictWebsites(userInteract, indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 3))
+    predictDict[3] = predictWebsites(userInteract, indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 4))
+    predictDict[4] = predictWebsites(userInteract, indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 5))
+    predictDict[5] = predictWebsites(userInteract, indexOfclass+10001)
+    indexOfclass = np.asarray(np.where(kmeanLabels == 6))
+    predictDict[6]= predictWebsites(userInteract, indexOfclass+10001)
+    centroids=kmeans.cluster_centers_
+
+    # Testing
+    testCosine = cosine_similarity(X_test, centroids)
+    indx = np.argsort(testCosine, axis=1)
+    finalIndex = indx[:, -1:]
+    truPredict=0
+
+    for clustNum,target in zip(finalIndex,testUser.values()):
+        for web in predictDict[int(clustNum)]:
+                if int(web)==int(target):
+                    truPredict+=1
+
+    testAccuracy=truPredict/5000
+
+    print('Test accuracy=')
+    print(testAccuracy)
 
 
-def hierachicalClustering(X_train, X_val, y_train, y_val):
-    """
-    Hierarchical implementation
-    :return:
-    """
-    clustering = AgglomerativeClustering().fit(X_train)
-    labels = clustering.labels_
-    indexOfclass = np.where(labels == 0)
-    clusterOne = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 1)
-    clusterTwo = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 2)
-    clusterThree = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 3)
-    clusterFour = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 4)
-    clusterFive = y_train[indexOfclass]
-    y_pred = clustering.fit_predict(X_val)
-    return y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive
+    elbowPlot(kmeans,X_train)
 
-def dbscanClustering(X_train, X_val, y_train, y_val):
-    """
-    DBSCAN implementation
-    :return:
-    """
-    db = DBSCAN(eps=0.3, min_samples=10).fit(X_train)
-    labels = db.labels_
-    indexOfclass = np.where(labels == 0)
-    clusterOne = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 1)
-    clusterTwo = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 2)
-    clusterThree = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 3)
-    clusterFour = y_train[indexOfclass]
-    indexOfclass = np.where(labels == 4)
-    clusterFive = y_train[indexOfclass]
-    y_pred = db.fit_predict(X_val)
-    return y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive
 
-def predictWebsites(userInteraction,y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive):
+def elbowPlot(model,data):
+    visualizer = KElbowVisualizer(model, k=(1, 12))
+    visualizer.fit(data)
+    visualizer.show()
+
+
+
+
+def predictWebsites(userInteraction,clusterWeb):
     """
     Prediction
     :return:
     """
     webList=[]
-    count=0
-    backup=[]
-    if y_pred[0]==0:
-        for user in clusterOne:
-            if count==0:
-                webList=userInteraction[user]
-                backup=webList
-                count=1
-            else:
-                webList=set(webList) & set(userInteraction[user])
-            if len(webList)==0:
-                webList=backup
 
-    if y_pred[0] == 1:
-        for user in clusterTwo:
-            if count == 0:
-                webList = userInteraction[user]
-                backup = webList
-                count = 1
-            else:
-                webList = set(webList) & set(userInteraction[user])
+    for user in clusterWeb[0]:
+        webList.append(userInteraction[user])
+    flat_list = [item for sublist in webList for item in sublist]
+    count = dict(Counter(flat_list))
+    clusteList=sorted(count.items(), key=lambda kv: kv[1])
+    finalList=[]
+    for web,count in clusteList[len(clusteList)-3:]:
+        finalList.append(web)
+    # singleMax=max(count.items(), key=operator.itemgetter(1))[0]
+    return (finalList)
 
-            if len(webList)==0:
-                webList=backup
 
-    if y_pred[0] == 2:
-        for user in clusterThree:
-            if count == 0:
-                webList = userInteraction[user]
-                backup = webList
-                count = 1
-            else:
-                webList = set(webList) & set(userInteraction[user])
-
-            if len(webList)==0:
-                webList=backup
-
-    if y_pred[0] == 3:
-        for user in clusterFour:
-            if count == 0:
-                webList = userInteraction[user]
-                backup = webList
-                count = 1
-            else:
-                webList = set(webList) & set(userInteraction[user])
-                if len(webList) == 0:
-                    webList = backup
-    if y_pred[0] == 4:
-        for user in clusterFive:
-            if count == 0:
-                webList = userInteraction[user]
-                backup = webList
-                count = 1
-            else:
-                webList = set(webList) & set(userInteraction[user])
-            if len(webList)==0:
-                webList=backup
-    return webList
 
 if __name__ == '__main__':
     """
     Main function 
     
     """
-    userWeb,userInteraction = dataPreprocessing()
+    userWeb,userInteraction = dataPreprocessing('training.txt')
     users = np.array(list(userWeb.keys()))
     userValues = np.array(list(userWeb.values()))
-    X_train, X_val, y_train, y_val = train_test_split(userValues, users, test_size = 0.3, random_state = 42)
+    testuserWeb,testLabel=dataPreprocessing('test.txt')
+    testusers = np.array(list(testuserWeb.keys()))
+    testuserValues = np.array(list(testuserWeb.values()))
 
-    y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive=kmeanClustering(X_train, X_val, y_train, y_val)
-    predicteOutput= predictWebsites(userInteraction,y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive)
-    print(predicteOutput)
-    y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive =hierachicalClustering(X_train, X_val, y_train, y_val)
-    predicteOutput = predictWebsites(userInteraction, y_pred, clusterOne, clusterTwo, clusterThree, clusterFour,
-                                     clusterFive)
-    print(predicteOutput)
-    y_pred, clusterOne, clusterTwo, clusterThree, clusterFour, clusterFive = dbscanClustering(X_train, X_val, y_train, y_val)
-    predicteOutput = predictWebsites(userInteraction, y_pred, clusterOne, clusterTwo, clusterThree, clusterFour,
-                                     clusterFive)
-    print(predicteOutput)
+    # User-based neighboring
+    userBasedNeighboring(userValues,userInteraction,testuserValues,testLabel)
 
+    # k-mean clustering
+    kmeanClustering(userValues,userInteraction,testuserValues,testLabel)
